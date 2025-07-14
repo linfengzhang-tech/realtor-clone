85002da8ef4fc77ca18494a2ed9f97155b8c581b
Linfeng Zhang <lzhang@trellis.co>
2025-07-14 18:06:25 -0500

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+const Header = () => {
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  const isActive = (path) => pathname === path;
+
+  return (
+    <div className="bg-white shadow-sm border-b sticky top-0 z-50">
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <div>
+            <img className="h-5 cursor-pointer" src="/logo-brand.svg" alt="Logo" />
+        </div>
+
+        <div>
+            <ul className="flex items-center space-x-10">
+                <li className={`py-3 border-b-[3px] border-b-transparent text-sm text-gray-600 hover:text-red-500 transition duration-300 ${isActive('/') ? 'text-black border-b-red-500 pointer-events-none' : 'text-gray-500'}`}>
+                    <Link to="/">Home</Link>
+                </li>
+                <li className={`py-3 border-b-[3px] border-b-transparent text-sm text-gray-600 hover:text-red-500 transition duration-300 ${isActive('/offers') ? 'text-black border-b-red-500 pointer-events-none' : 'text-gray-500'}`}>
+                    <Link to="/offers">Offers</Link>
+                </li>
+                <li className={`py-3 border-b-[3px] border-b-transparent text-sm text-gray-600 hover:text-red-500 transition duration-300 ${isActive('/sign-in') ? 'text-black border-b-red-500 pointer-events-none' : 'text-gray-500'}`}>
+                    <Link to="/sign-in">Sign in</Link>
+                </li>
+            </ul>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
