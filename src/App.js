b3a6c94a921f99d42fb84e19cc7af30f843d0efd
Linfeng Zhang <lzhang@trellis.co>
2025-07-14 15:38:22 -0500

    create page and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
+      </Routes>
+    </Router>
   );
 }
 
